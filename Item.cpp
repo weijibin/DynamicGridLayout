1ce3ba3dae3307144d2f5144019959fbaddbfc4c
weijibin <1285253265@qq.com>
2020-01-05 21:38:19 +0800

    fixbug

--- Item.cpp
+++ Item.cpp
@@ -11,6 +11,13 @@ Item::Item(QWidget *parent) :
 {
     ui->setupUi(this);
     this->setFixedSize(300,300);
+
+    this->setObjectName("itemObj");
+    QString str;
+    str = "QWidget#itemObj{background-color:#fff000;}\
+           QWidget#itemObj[status=\"1\"]{background-color:#ff0000;}\
+           QWidget#itemObj[status=\"2\"]{background-color:#00ff00;}";
+    this->setStyleSheet(str);
 }
 
 Item::~Item()
@@ -24,4 +31,25 @@ void Item::paintEvent(QPaintEvent *event)
     opt.init(this);
     QPainter p(this);
     style()->drawPrimitive(QStyle::PE_Widget, &opt, &p, this);
+}
+
+void Item::enterEvent(QEvent *event)
+{
+    qDebug() << Q_FUNC_INFO;
+    setStatus(2);
+}
+
+void Item::leaveEvent(QEvent *event)
+{
+    qDebug() << Q_FUNC_INFO;
+    setStatus(1);
+}
+
+void Item::setStatus(int status)
+{
+    QString str = QString::number(status);
+    qDebug() << Q_FUNC_INFO << QString::number(status);
+    this->setProperty("status",status);
+    this->style()->unpolish(this);
+    this->style()->polish(this);
 }
--- Item.h
+++ Item.h
@@ -17,8 +17,13 @@ public:
 
 protected:
     virtual void paintEvent(QPaintEvent *event) override;
+    virtual void enterEvent(QEvent *event) override;
+    virtual void leaveEvent(QEvent *event) override;
+private:
+    void setStatus(int status);
 private:
     Ui::Item *ui;
+    int  m_status;
 };
 
 #endif // ITEM_H
